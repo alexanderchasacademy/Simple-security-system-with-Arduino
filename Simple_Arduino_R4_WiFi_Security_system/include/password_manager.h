e32aded64f7b04522079c7b033c7374260d9d49f
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-07-08 18:55:10 +0200

    added password_manager .h/.cpp

---
+++ Simple_Arduino_R4_WiFi_Security_system/include/password_manager.h
@@ -0,0 +1,13 @@
+#ifndef PASSWORD_MANAGER_H
+#define PASSWORD_MANAGER_H
+
+#include <Arduino.h>
+#include "secrets.h"
+#include "keypad_manager.h"
+#include "LCD_display.h"
+
+extern byte password_index;
+extern bool correct_password;
+
+
+#endif
---
+++ Simple_Arduino_R4_WiFi_Security_system/src/password_manager.cpp
@@ -0,0 +1,4 @@
+#include "password_manager.h"
+
+byte password_index = 0;
+bool correct_password = false;
